3007897c4054935dcb9ec5c49f5289ff9222a9d1
= <=>
2019-12-31 15:54:47 +0700

    update

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,16 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(value="com.example.repository")
+@ComponentScan(value="com.example.resources")
+@ComponentScan(value="com.example.service")
+@EntityScan(value="com.example.model")
 @SpringBootApplication
 public class DemoApplication {
 
