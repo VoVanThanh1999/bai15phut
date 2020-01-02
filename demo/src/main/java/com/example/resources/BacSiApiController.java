132675c55082f16010fe57fd69d96201986dab01
Kendeptrai <star.wonders.121@gmail.com>
2020-01-02 16:10:28 +0700

    update cua viet mat lx

--- demo/src/main/java/com/example/resources/BacSiApiController.java
+++ demo/src/main/java/com/example/resources/BacSiApiController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,21 +17,32 @@ import com.example.service.BacSiService;
 @RestController
 @RequestMapping(value = "/api")
 public class BacSiApiController {
-	
+
 	@Autowired
 	BacSiService bacSiService;
+
 	@RequestMapping(value = "/getAll")
-	public List<BacSi>getAll(){
+	public List<BacSi> getAll() {
 		return bacSiService.getAll();
 	}
-	@RequestMapping(value = "/delete/{id}",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public boolean delete(@PathVariable("id") int id) {
 		bacSiService.delete(id);
 		return true;
 	}
-	@RequestMapping(value = "/add")
-	public boolean add(@RequestBody BacSi bacSi) {
-		bacSiService.add(bacSi);
-		return true;
+
+	@PostMapping(value = "/add")
+	public String add(@RequestBody BacSi bacSi) {
+		System.err.println(bacSi.getMaBacSi());
+		 bacSiService.save(bacSi); 
+		return "them thanh cong";
+	}
+	
+	@PutMapping(value = "/edit")
+	public String edit(@RequestBody BacSi bacSi) {
+		System.err.println(bacSi.getMaBacSi());
+		 bacSiService.edit(bacSi); 
+		return " sua thanh cong";
 	}
 }
